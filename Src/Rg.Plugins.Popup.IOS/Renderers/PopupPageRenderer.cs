7ab3cc728ff3d64a382727ddb021bcefd18e698e
rotorgames <rotorgames@bk.ru>
2016-02-22 00:43:56 +0300

    feat(Popup, iOS): Moved popup when showed keyboard

--- Src/Rg.Plugins.Popup.IOS/Renderers/PopupPageRenderer.cs
+++ Src/Rg.Plugins.Popup.IOS/Renderers/PopupPageRenderer.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Foundation;
 using Rg.Plugins.Popup.IOS.Renderers;
 using Rg.Plugins.Popup.Pages;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
+using Size = Xamarin.Forms.Size;
 
 [assembly: ExportRenderer(typeof(PopupPage), typeof(PopupPageRenderer))]
 namespace Rg.Plugins.Popup.IOS.Renderers
@@ -24,6 +23,35 @@ namespace Rg.Plugins.Popup.IOS.Renderers
         {
             base.ViewDidLayoutSubviews();
             SetElementSize(new Size(View.Bounds.Width, View.Bounds.Height));
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            NSNotificationCenter.DefaultCenter.AddObserver(UIKeyboard.WillChangeFrameNotification, KeyBoardUpNotification);
+            NSNotificationCenter.DefaultCenter.AddObserver(UIKeyboard.WillHideNotification,KeyBoardDownNotification);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            NSNotificationCenter.DefaultCenter.RemoveObserver(this);
+        }
+
+        private void KeyBoardUpNotification(NSNotification notifi)
+        {
+            var r = UIKeyboard.BoundsFromNotification(notifi);
+            var screen = UIScreen.MainScreen.Bounds;
+
+            SetElementSize(new Size(r.Width, screen.Height - r.Height));
+        }
+
+        private void KeyBoardDownNotification(NSNotification notifi)
+        {
+            var r = UIKeyboard.BoundsFromNotification(notifi);
+            var screen = UIScreen.MainScreen.Bounds;
+
+            SetElementSize(new Size(r.Width, screen.Height));
         }
     }
 }
