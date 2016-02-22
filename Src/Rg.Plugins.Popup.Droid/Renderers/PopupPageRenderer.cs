320f8e4fd755a3c49333b802d764240d1d0b6df7
rotorgames <rotorgames@bk.ru>
2016-02-22 11:43:23 +0300

    fix(Popup, Android, Keyboard): Keyboard not hided when entry unfocused

--- Src/Rg.Plugins.Popup.Droid/Renderers/PopupPageRenderer.cs
+++ Src/Rg.Plugins.Popup.Droid/Renderers/PopupPageRenderer.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -20,6 +21,9 @@ namespace Rg.Plugins.Popup.Droid.Renderers
 {
     class PopupPageRenderer : PageRenderer
     {
+        private DateTime downTime;
+        private Xamarin.Forms.Point downPosition;
+
         protected override void OnLayout(bool changed, int l, int t, int r, int b)
         {
             Element.ForceLayout();
@@ -40,6 +44,37 @@ namespace Rg.Plugins.Popup.Droid.Renderers
                 return false;
             });
             base.OnDetachedFromWindow();
+        }
+
+        public override bool DispatchTouchEvent(MotionEvent e)
+        {
+            if (e.Action == MotionEventActions.Down)
+            {
+                downTime = DateTime.UtcNow;
+                downPosition = new Xamarin.Forms.Point((double)e.RawX, (double)e.RawY);
+            }
+            if (e.Action != MotionEventActions.Up)
+                return base.DispatchTouchEvent(e);
+            Android.Views.View currentFocus1 = ((Activity) Context).CurrentFocus;
+            bool flag = base.DispatchTouchEvent(e);
+            if (currentFocus1 is EditText)
+            {
+                Android.Views.View currentFocus2 = ((Activity) Context).CurrentFocus;
+                if (currentFocus1 == currentFocus2 && this.downPosition.Distance(new Xamarin.Forms.Point((double)e.RawX, (double)e.RawY)) <= ContextExtensions.ToPixels(Context, 20.0) && !(DateTime.UtcNow - downTime > TimeSpan.FromMilliseconds(200.0)))
+                {
+                    int[] location = new int[2];
+                    currentFocus1.GetLocationOnScreen(location);
+                    float num1 = e.RawX + currentFocus1.Left - location[0];
+                    float num2 = e.RawY + currentFocus1.Top - location[1];
+                    if (!new Rectangle((double)currentFocus1.Left, (double)currentFocus1.Top, (double)currentFocus1.Width, (double)currentFocus1.Height).Contains((double)num1, (double)num2))
+                    {
+                        ContextExtensions.HideKeyboard(Context, currentFocus1);
+                        RequestFocus();
+                        currentFocus1.ClearFocus();
+                    }
+                }
+            }
+            return flag;
         }
     }
 }
