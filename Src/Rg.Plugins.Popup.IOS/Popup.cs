1db89f52f53335e132dc172da2c860d2f5120654
rotorgames <rotorgames@bk.ru>
2016-02-28 00:32:48 +0300

    fix(ios-linker): Test fix ios linker. See #3

--- Src/Rg.Plugins.Popup.IOS/Popup.cs
+++ Src/Rg.Plugins.Popup.IOS/Popup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Rg.Plugins.Popup.IOS.Impl;
 using Rg.Plugins.Popup.Services;
 
 namespace Rg.Plugins.Popup.IOS
@@ -10,6 +11,9 @@ namespace Rg.Plugins.Popup.IOS
         public static void Init()
         {
             // Fix sdk linker
+            var temp = DateTime.Now;
+            var t = typeof (PopupNavigationIOS);
+            var t2 = typeof (ScreenHelperIos);
         }
     }
 }
