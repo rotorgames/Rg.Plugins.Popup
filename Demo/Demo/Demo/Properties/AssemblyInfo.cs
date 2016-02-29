3a3b5a0b732e446b1b2fbfe1bb1e4cc21ca14f1f
rotorgames <rotorgames@bk.ru>
2016-02-29 18:29:05 +0300

    fix(AssemblyInfo): remove Demo using

--- Demo/Demo/Demo/Properties/AssemblyInfo.cs
+++ Demo/Demo/Demo/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Demo;
+using Rg.Plugins.Popup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
