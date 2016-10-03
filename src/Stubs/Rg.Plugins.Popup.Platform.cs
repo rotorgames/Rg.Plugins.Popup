6c34db509e7982ac290a76d4c54a34a44351d0b7
Kirill <rotorgames@bk.ru>
2016-10-03 18:29:04 +0400

    refact(Stubs, UWP, WP): Deleted WP and UWP stubs projects

--- src/Stubs/Rg.Plugins.Popup.Platform.cs
+++ src/Stubs/Rg.Plugins.Popup.Platform.cs
@@ -3,8 +3,6 @@
 using Rg.Plugins.Popup.Droid.Renderers;
 #elif __IOS__
 using Rg.Plugins.Popup.IOS.Renderers;
-#elif __WP__ || __UWP__
-using Rg.Plugins.Popup.Windows.Renderers;
 #endif
 using Xamarin.Forms;
 
