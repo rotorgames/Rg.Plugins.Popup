344427e06f7ac1e41cb56aee8235b2ce09981e36
Kirill <rotorgames@bk.ru>
2016-10-05 20:56:10 +0400

    feat(PreserveAttributes, Android, iOS): Add Preserve attribute to assembly

--- src/Demo/Demo.Droid/Properties/AssemblyInfo.cs
+++ src/Demo/Demo.Droid/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
+using Xamarin.Forms.Internals;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -32,3 +32,4 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: Preserve]
--- src/Demo/Demo/Properties/AssemblyInfo.cs
+++ src/Demo/Demo/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Demo;
+using Xamarin.Forms.Internals;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -29,3 +30,4 @@ using Demo;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Preserve]
