fdca1005e9c432a146347b1331948b96375da7d8
Kirill Lyubimov <dev.lyubimov.kirill@gmail.com>
2017-11-23 18:25:24 +0400

    fix(PopupPlatform, iOS): Сhildren of PopupPage was not released from memory if them was removed before page was popped

--- src/Rg.Plugins.Popup/Extensions/VisualElementExtensions.cs
+++ src/Rg.Plugins.Popup/Extensions/VisualElementExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
@@ -6,6 +7,7 @@ namespace Rg.Plugins.Popup.Extensions
 {
     internal static class VisualElementExtensions
     {
+        [Obsolete("Use "+nameof(Element)+"."+nameof(Element.Descendants))]
         internal static IEnumerable<Element> RgDescendants(this Element element)
         {
             var queue = new Queue<Element>(16);
