b8deca73a5e91d3daf6d8017af684e62d2f97eca
Kirill Lyubimov <dev.lyubimov.kirill@gmail.com>
2018-07-16 10:33:08 +0400

    fix(PopupWindow): Fixed Failed to marshal the Objective-C object. See #307

--- src/Rg.Plugins.Popup.IOS/Platform/PopupWindow.cs
+++ src/Rg.Plugins.Popup.IOS/Platform/PopupWindow.cs
@@ -2,11 +2,24 @@
 using CoreGraphics;
 using UIKit;
 using Rg.Plugins.Popup.Pages;
+using Foundation;
 
 namespace Rg.Plugins.Popup.IOS.Platform
 {
+    [Preserve(AllMembers = true)]
+    [Register("RgPopupWindow")]
     internal class PopupWindow : UIWindow
     {
+        public PopupWindow(IntPtr handle):base(handle)
+        {
+            // Fix #307
+        }
+
+        public PopupWindow()
+        {
+
+        }
+
         public override UIView HitTest(CGPoint point, UIEvent uievent)
         {
             var platformRenderer = (PopupPlatformRenderer) RootViewController;
