57ddd085b29235b5585e46af4d8805ae12f89cdb
Martijn van Dijk <mhvdijk@gmail.com>
2019-10-04 17:20:35 +0200

    Initial conversion

--- src/Rg.Plugins.Popup/Properties/GlobalAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("rotorgames")]
-[assembly: AssemblyProduct("Rg.Plugins.Popup")]
-[assembly: AssemblyCopyright("Copyright © rotorgames 2015-2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
