594154a1f1b031a631f25243ef408373f7054933
Sergiu Bulzan <sergiu.bulzan@yardi.com>
2020-03-07 16:20:47 +0200

    Forwards PreferredStatusBarStyle and PrefersStatusBarHidden from PopupPlatformRenderer to the PopupPageRenderer allowing customisation. Updates Demo.iOS info.plist to UIViewControllerBasedStatusBarAppearance = YES Adds LoginPageRenderer in Demo.iOS to illustrate change in status bar color

---
+++ src/Demo/Demo.iOS/LoginPopupPageRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Demo.iOS;
+using Demo.Pages;
+using Rg.Plugins.Popup.IOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(LoginPopupPage), typeof(LoginPopupPageRenderer))]
+namespace Demo.iOS
+{
+    public class LoginPopupPageRenderer : PopupPageRenderer
+    { 
+        public override bool PrefersStatusBarHidden()
+        {
+            return false; 
+        }
+
+        public override UIStatusBarStyle PreferredStatusBarStyle()
+        {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0) && TraitCollection.UserInterfaceStyle == UIUserInterfaceStyle.Dark)
+            {
+                return UIStatusBarStyle.DarkContent;
+            }
+            return UIStatusBarStyle.LightContent; 
+        }
+    }
+}
