77cade5155ff4c6a622b8e2dbaa14ae5a44dbe95
Martijn van Dijk <mhvdijk@gmail.com>
2020-04-08 18:10:45 +0200

    Add mac sample

--- Samples/Demo.Mac/AppDelegate.cs
+++ Samples/Demo.Mac/AppDelegate.cs
@@ -1,23 +1,35 @@
 ﻿using AppKit;
 using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
 
 namespace Demo.Mac
 {
     [Register("AppDelegate")]
-    public class AppDelegate : NSApplicationDelegate
+    public class AppDelegate : FormsApplicationDelegate
     {
         public AppDelegate()
         {
+            const NSWindowStyle style = NSWindowStyle.Titled | NSWindowStyle.Closable | NSWindowStyle.Miniaturizable | NSWindowStyle.Resizable;
+
+            var rect = new CoreGraphics.CGRect(200, 200, 1024, 768);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "Demo",
+                TitleVisibility = NSWindowTitleVisibility.Hidden
+            };
+            MainWindow.Center();
+            NSApplication.SharedApplication.MainMenu = new NSMenu();
         }
+
+        public sealed override NSWindow MainWindow { get; }
 
         public override void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to initialize your application
-        }
-
-        public override void WillTerminate(NSNotification notification)
-        {
-            // Insert code here to tear down your application
+            Rg.Plugins.Popup.Popup.Init();
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
         }
     }
 }
