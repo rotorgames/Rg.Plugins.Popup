ba963e88d0f5dff3bec5b66edb025312c54059ef
Dan Siegel <me@dansiegel.net>
2020-10-07 11:41:14 -0700

    add XmlnsDefinition

---
+++ Rg.Plugins.Popup/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using Xamarin.Forms;
+
+[assembly: XmlnsDefinition("http://rotorgames.com", "Rg.Plugins.Popup.Animations")]
+[assembly: XmlnsDefinition("http://rotorgames.com", "Rg.Plugins.Popup.Pages")]
+[assembly: XmlnsPrefix("http://rotorgames.com", "rg")]
